6a691bf44ea1c3667a02826255242b9475affa5f
joegotflow83 <joe@absolutod.com>
2016-07-31 21:21:22 -0400

    working on check_guess fn

--- lib/hangman/main.ex
+++ lib/hangman/main.ex
@@ -8,6 +8,29 @@ defmodule Hangman.Main do
     len = String.length word
     String.duplicate("_", len)
   end
+
+  def user_guess do
+    guess = IO.gets "Guess a letter: "
+    String.replace(guess, "\n", "")
+  end
+
+  def check_guess(guess, word)do
+    case guess do
+      "o" ->
+        IO.puts "You guessed correctly"
+      "m" ->
+        IO.puts "You guessed correctly"
+    end
+  end
+
+  def round do
+    word = gen_word
+    |> blank_word
+    |> IO.puts
+    guess = user_guess
+    word = Enum.
+    check_guess(guess, word)
+  end
 end
 
 #def blank_word do
